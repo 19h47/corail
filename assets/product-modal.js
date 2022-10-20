2bf4f8df71edb2b97d3da62d76a690e0c3cbdfef
19h47 <jeremylevron@19h47.fr>
2022-10-20 14:52:36 +0200

    :construction: Swiper

--- assets/product-modal.js
+++ assets/product-modal.js
@@ -1,34 +1,41 @@
-if (!customElements.get('product-modal')) {
-  customElements.define('product-modal', class ProductModal extends ModalDialog {
-    constructor() {
-      super();
-    }
+if (!customElements.get("product-modal")) {
+	class ProductModal extends ModalDialog {
+		hide() {
+			super.hide();
+		}
 
-    hide() {
-      super.hide();
-    }
+		show(opener) {
+			super.show(opener);
+			this.showActiveMedia();
+		}
 
-    show(opener) {
-      super.show(opener);
-      this.showActiveMedia();
-    }
+		showActiveMedia() {
+			this.querySelectorAll(
+				`[data-media-id]:not([data-media-id="${this.openedBy.getAttribute("data-media-id")}"])`
+			).forEach(element => element.classList.remove("active"));
 
-    showActiveMedia() {
-      this.querySelectorAll(`[data-media-id]:not([data-media-id="${this.openedBy.getAttribute("data-media-id")}"])`).forEach((element) => {
-          element.classList.remove('active');
-        }
-      )
-      const activeMedia = this.querySelector(`[data-media-id="${this.openedBy.getAttribute("data-media-id")}"]`);
-      const activeMediaTemplate = activeMedia.querySelector('template');
-      const activeMediaContent = activeMediaTemplate ? activeMediaTemplate.content : null;
-      activeMedia.classList.add('active');
-      activeMedia.scrollIntoView();
+			const activeMedia = this.querySelector(
+				`[data-media-id="${this.openedBy.getAttribute("data-media-id")}"]`
+			);
+			const activeMediaTemplate = activeMedia.querySelector("template");
+			const activeMediaContent = activeMediaTemplate ? activeMediaTemplate.content : null;
 
-      const container = this.querySelector('[role="document"]');
-      container.scrollLeft = (activeMedia.width - container.clientWidth) / 2;
+			activeMedia.classList.add("active");
+			activeMedia.scrollIntoView();
 
-      if (activeMedia.nodeName == 'DEFERRED-MEDIA' && activeMediaContent && activeMediaContent.querySelector('.js-youtube'))
-        activeMedia.loadContent();
-    }
-  });
+			const container = this.querySelector('[role="document"]');
+
+			container.scrollLeft = (activeMedia.width - container.clientWidth) / 2;
+
+			if (
+				"DEFERRED-MEDIA" === activeMedia.nodeName &&
+				activeMediaContent &&
+				activeMediaContent.querySelector(".js-youtube")
+			) {
+				activeMedia.loadContent();
+			}
+		}
+	}
+
+	customElements.define("product-modal", ProductModal);
 }
---
+++ assets/slider.js
@@ -0,0 +1,127 @@
+/* global Swiper */
+// if (!customElements.get("slider-component")) {
+// 	class SliderComponent extends HTMLElement {
+// 		constructor() {
+// 			super();
+// 			this.slider = this.querySelector('[id^="Slider-"]');
+// 			this.sliderItems = this.querySelectorAll('[id^="Slide-"]');
+// 			this.enableSliderLooping = false;
+// 			this.currentPageElement = this.querySelector(".js-current");
+// 			this.pageTotalElement = this.querySelector(".js-total");
+// 			this.prevButton = this.querySelector('button[name="previous"]');
+// 			this.nextButton = this.querySelector('button[name="next"]');
+
+// 			if (!this.slider || !this.nextButton) return;
+
+// 			this.initPages();
+// 			const resizeObserver = new ResizeObserver((entries) => this.initPages());
+// 			resizeObserver.observe(this.slider);
+
+// 			this.slider.addEventListener("scroll", this.update.bind(this));
+// 			this.prevButton.addEventListener("click", this.onButtonClick.bind(this));
+// 			this.nextButton.addEventListener("click", this.onButtonClick.bind(this));
+// 		}
+
+// 		initPages() {
+// 			this.sliderItemsToShow = [...this.sliderItems].filter(element => 0 < element.clientWidth);
+
+// 			if (2 > this.sliderItemsToShow.length) {
+// 				return;
+// 			}
+
+// 			this.sliderItemOffset =
+// 				this.sliderItemsToShow[1].offsetLeft - this.sliderItemsToShow[0].offsetLeft;
+// 			this.slidesPerPage = Math.floor(
+// 				(this.slider.clientWidth - this.sliderItemsToShow[0].offsetLeft) / this.sliderItemOffset
+// 			);
+// 			this.totalPages = this.sliderItemsToShow.length - this.slidesPerPage + 1;
+// 			this.update();
+// 		}
+
+// 		resetPages() {
+// 			this.sliderItems = this.querySelectorAll('[id^="Slide-"]');
+// 			this.initPages();
+// 		}
+
+// 		update() {
+// 			const previousPage = this.currentPage;
+// 			this.currentPage = Math.round(this.slider.scrollLeft / this.sliderItemOffset) + 1;
+
+// 			if (this.currentPageElement && this.pageTotalElement) {
+// 				this.currentPageElement.textContent = this.currentPage;
+// 				this.pageTotalElement.textContent = this.totalPages;
+// 			}
+
+// 			if (this.currentPage !== previousPage) {
+// 				this.dispatchEvent(
+// 					new CustomEvent("slideChanged", {
+// 						detail: {
+// 							currentPage: this.currentPage,
+// 							currentElement: this.sliderItemsToShow[this.currentPage - 1],
+// 						},
+// 					})
+// 				);
+// 			}
+
+// 			if (this.enableSliderLooping) return;
+
+// 			if (this.isSlideVisible(this.sliderItemsToShow[0]) && 0 === this.slider.scrollLeft) {
+// 				this.prevButton.setAttribute("disabled", "disabled");
+// 			} else {
+// 				this.prevButton.removeAttribute("disabled");
+// 			}
+
+// 			if (this.isSlideVisible(this.sliderItemsToShow[this.sliderItemsToShow.length - 1])) {
+// 				this.nextButton.setAttribute("disabled", "disabled");
+// 			} else {
+// 				this.nextButton.removeAttribute("disabled");
+// 			}
+// 		}
+
+// 		isSlideVisible(element, offset = 0) {
+// 			const lastVisibleSlide = this.slider.clientWidth + this.slider.scrollLeft - offset;
+// 			return (
+// 				element.offsetLeft + element.clientWidth <= lastVisibleSlide &&
+// 				element.offsetLeft >= this.slider.scrollLeft
+// 			);
+// 		}
+
+// 		onButtonClick(event) {
+// 			event.preventDefault();
+// 			const step = event.currentTarget.dataset.step || 1;
+// 			this.slideScrollPosition =
+// 				"next" === event.currentTarget.name
+// 					? this.slider.scrollLeft + step * this.sliderItemOffset
+// 					: this.slider.scrollLeft - step * this.sliderItemOffset;
+// 			this.slider.scrollTo({
+// 				left: this.slideScrollPosition,
+// 			});
+// 		}
+// 	}
+
+// 	customElements.define("slider-component", SliderComponent);
+// }
+
+if (!customElements.get("slider-component")) {
+	class SliderComponent extends HTMLElement {
+		constructor() {
+			super();
+
+			const parameters = JSON.parse(this.getAttribute('data-parameters')) || {};
+
+			// if (parameters.pagination) {
+			// 	[parameters.pagination.el] = this.$('pagination')
+			// }
+
+			console.log(this);
+
+			this.swiper = new Swiper(this, {
+				...{ slidesPerView: 'auto' },
+				...parameters,
+			});
+		}
+	}
+
+	customElements.define("slider-component", SliderComponent);
+}
+
