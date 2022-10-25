d4e4360654d849148a3592e374f36f8c8f16b3cf
19h47 <jeremylevron@19h47.fr>
2022-10-25 17:54:12 +0200

    :sparkles: New block product

--- assets/quantity-input.js
+++ assets/quantity-input.js
@@ -1,30 +1,33 @@
-class QuantityInput extends HTMLElement {
-	constructor() {
-		super();
-		this.input = this.querySelector("input");
-		this.changeEvent = new Event("change", { bubbles: true });
+if (!customElements.get('quantity-input')) {
+	class QuantityInput extends HTMLElement {
+		constructor() {
+			super();
+			this.input = this.querySelector("input");
+			this.changeEvent = new Event("change", { bubbles: true });
 
-		this.querySelectorAll("button").forEach(button => button.addEventListener("click", this.onButtonClick.bind(this)));
+			this.querySelectorAll("button").forEach(button => button.addEventListener("click", this.onButtonClick.bind(this)));
+		}
+
+		onButtonClick(event) {
+			event.preventDefault();
+
+			const previousValue = this.input.value;
+
+			if ("plus" === event.target.name) {
+				this.input.stepUp();
+			}
+
+			if ("minus" === event.target.name) {
+				this.input.stepDown();
+			}
+
+			console.log(event.target.name, previousValue, this.input.value);
+
+			if (previousValue !== this.input.value) {
+				this.input.dispatchEvent(this.changeEvent);
+			}
+		}
 	}
 
-	onButtonClick(event) {
-		console.log(event.target.name);
-
-		event.preventDefault();
-		const previousValue = this.input.value;
-
-		if ("plus" === event.target.name) {
-			this.input.stepUp();
-		}
-
-		if ("minus" === event.target.name) {
-			this.input.stepDown();
-		}
-
-		if (previousValue !== this.input.value) {
-			this.input.dispatchEvent(this.changeEvent);
-		}
-	}
+	customElements.define("quantity-input", QuantityInput);
 }
-
-customElements.define("quantity-input", QuantityInput);
