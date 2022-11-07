590355080df29f7d85f0142ca722e8481186262d
19h47 <jeremylevron@19h47.fr>
2022-11-07 10:59:13 +0100

    :sparkles: Login

--- assets/customer.js
+++ assets/customer.js
@@ -1,3 +1,5 @@
+/* global Shopify */
+
 const selectors = {
 	customerAddresses: '[data-customer-addresses]',
 	addressCountrySelect: '[data-address-country-select]',
@@ -12,14 +14,19 @@ const attributes = {
 	confirmMessage: 'data-confirm-message'
 };
 
+// eslint-disable-next-line no-unused-vars
 class CustomerAddresses {
 	constructor() {
+		// eslint-disable-next-line no-underscore-dangle
 		this.elements = this._getElements();
-		if (Object.keys(this.elements).length === 0) return;
+		if (0 === Object.keys(this.elements).length) return;
+		// eslint-disable-next-line no-underscore-dangle
 		this._setupCountries();
+		// eslint-disable-next-line no-underscore-dangle
 		this._setupEventListeners();
 	}
 
+	// eslint-disable-next-line no-underscore-dangle, class-methods-use-this
 	_getElements() {
 		const container = document.querySelector(selectors.customerAddresses);
 		return container ? {
@@ -32,14 +39,15 @@ class CustomerAddresses {
 		} : {};
 	}
 
+	// eslint-disable-next-line no-underscore-dangle
 	_setupCountries() {
 		if (Shopify && Shopify.CountryProvinceSelector) {
 			// eslint-disable-next-line no-new
 			new Shopify.CountryProvinceSelector('AddressCountryNew', 'AddressProvinceNew', {
 				hideElement: 'AddressProvinceContainerNew'
 			});
-			this.elements.countrySelects.forEach((select) => {
-				const formId = select.dataset.formId;
+			this.elements.countrySelects.forEach(select => {
+				const {formId} = select.dataset;
 				// eslint-disable-next-line no-new
 				new Shopify.CountryProvinceSelector(`AddressCountry_${formId}`, `AddressProvince_${formId}`, {
 					hideElement: `AddressProvinceContainer_${formId}`
@@ -48,30 +56,39 @@ class CustomerAddresses {
 		}
 	}
 
+	// eslint-disable-next-line no-underscore-dangle
 	_setupEventListeners() {
-		this.elements.toggleButtons.forEach((element) => {
+		this.elements.toggleButtons.forEach(element => {
+			// eslint-disable-next-line no-underscore-dangle
 			element.addEventListener('click', this._handleAddEditButtonClick);
 		});
-		this.elements.cancelButtons.forEach((element) => {
+
+		this.elements.cancelButtons.forEach(element => {
+			// eslint-disable-next-line no-underscore-dangle
 			element.addEventListener('click', this._handleCancelButtonClick);
 		});
-		this.elements.deleteButtons.forEach((element) => {
+
+		this.elements.deleteButtons.forEach(element => {
+			// eslint-disable-next-line no-underscore-dangle
 			element.addEventListener('click', this._handleDeleteButtonClick);
 		});
 	}
 
+	// eslint-disable-next-line no-underscore-dangle, class-methods-use-this
 	_toggleExpanded(target) {
 		target.setAttribute(
 			attributes.expanded,
-			(target.getAttribute(attributes.expanded) === 'false').toString()
+			('false' === target.getAttribute(attributes.expanded)).toString()
 		);
 	}
 
 	_handleAddEditButtonClick = ({ currentTarget }) => {
+		// eslint-disable-next-line no-underscore-dangle
 		this._toggleExpanded(currentTarget);
 	}
 
 	_handleCancelButtonClick = ({ currentTarget }) => {
+		// eslint-disable-next-line no-underscore-dangle
 		this._toggleExpanded(
 			currentTarget
 				.closest(selectors.addressContainer)
@@ -79,8 +96,9 @@ class CustomerAddresses {
 		)
 	}
 
+	// eslint-disable-next-line class-methods-use-this
 	_handleDeleteButtonClick = ({ currentTarget }) => {
-		// eslint-disable-next-line no-alert
+		// eslint-disable-next-line no-alert, no-restricted-globals
 		if (confirm(currentTarget.getAttribute(attributes.confirmMessage))) {
 			Shopify.postLink(currentTarget.dataset.target, {
 				parameters: { _method: 'delete' },
